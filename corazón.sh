8b078383c38125e28d451ec6bf8eee0fcea9cc94
Paula-Oreja <paula.oreja@alumnos.uneatlantico.es>
2025-04-07 07:07:45 +0000

    corazón

---
+++ corazón.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear
+echo ""
+sleep 0.3
+echo "      *     *"
+sleep 0.3
+echo "    ***   ***"
+sleep 0.3
+echo "   ***** *****"
+sleep 0.3
+echo "   ***********"
+sleep 0.3
+echo "    *********"
+sleep 0.3
+echo "     *******"
+sleep 0.3
+echo "      *****"
+sleep 0.3
+echo "       ***"
+sleep 0.3
+echo "        *"
+sleep 0.5
+echo ""
+echo "    💖 ¡Hecho con bash y amor! 💖"
